fd2d3a6d18278269ae17350dba14d464af7c6a50
GhostInn <112103443+GhostInn@users.noreply.github.com>
2024-05-31 22:37:08 +0300

    Update AppleTree.cs

--- Lab3/AppleTree.cs
+++ Lab3/AppleTree.cs
@@ -1,9 +1,7 @@
 namespace Lab3;
 
-// Объявляем класс AppleTree, который наследуется от базового класса Plant
 public class AppleTree : Plant
 {
-    // Конструктор класса AppleTree без параметров
     public AppleTree()
     {
         Sort = "сомнытэльно но окэй";
