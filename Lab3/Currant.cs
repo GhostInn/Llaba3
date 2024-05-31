36d16abaf1363d4d2682d7269fc91210e62bd933
GhostInn <112103443+GhostInn@users.noreply.github.com>
2024-05-31 22:37:30 +0300

    Update Currant.cs

--- Lab3/Currant.cs
+++ Lab3/Currant.cs
@@ -1,9 +1,7 @@
 namespace Lab3;
 
-// Объявляем класс Currant, который наследуется от базового класса Plant
 public class Currant : Plant
 {
-    // Конструктор класса Currant без параметров
     public Currant()
     {
         Sort = "вкусный";
