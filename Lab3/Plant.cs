da916425fd25f243cfc16b0efb9a500e165ac761
GhostInn <112103443+GhostInn@users.noreply.github.com>
2024-05-31 22:44:59 +0300

    Update Plant.cs

--- Lab3/Plant.cs
+++ Lab3/Plant.cs
@@ -1,8 +1,6 @@
 namespace Lab3;
 
-// Объявляем абстрактный класс Plant
 public abstract class Plant
 {
-    // Объявляем свойство для хранения сорта растения
     public string Sort { get; set; }
 }
